4ec2ba85599878c3ae3e7489d4af3d1628ade076
Marco Tijbout <32824067+MTijbout@users.noreply.github.com>
2019-07-13 13:53:57 +0200

    ljk

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 touch step1
 curl -o doInstall.sh https://raw.githubusercontent.com/MTijbout/public/master/doInstall.sh
+touch step2
 chmod +x doInstall.sh
+touch step3
 sudo ./doInstall.sh
+touch stepFinished
