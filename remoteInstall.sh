84debb0402bb9fd0f84cadce196f95c2ce551e64
Marco Tijbout <marco.tijbout@the-iot.cloud>
2019-07-13 21:31:48 +0200

    update

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-touch step1
 curl -o doInstall.sh https://raw.githubusercontent.com/MTijbout/public/master/doInstall.sh
-touch step2
 chmod +x doInstall.sh
-touch step3
 sudo ./doInstall.sh
-touch stepFinished
