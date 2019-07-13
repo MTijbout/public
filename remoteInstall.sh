4bfe560fc688d0dde2218561e0ec5a445adffd0d
Marco Tijbout <marco.tijbout@the-iot.cloud>
2019-07-13 13:50:07 +0200

    cache

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 touch step1
-curl -v -H "Cache-Control: no-cache" -o doInstall.sh https://raw.githubusercontent.com/MTijbout/public/master/doInstall.sh
+curl -v -o doInstall.sh https://raw.githubusercontent.com/MTijbout/public/master/doInstall.sh
 chmod +x doInstall.sh
 #sudo ./doInstall.sh
